cd332791be76310639b7321ccfd0187ecfa3aea6
ianschu <ianschu7@gmail.com>
2019-04-07 22:06:59 -0400

    added models, routes, and controllers folders

---
+++ book-searcher/client/build/precache-manifest.d72bb9b31b5e18f4fc933b747b516a43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f0b8f13b84c5940d4222",
+    "url": "/static/js/main.ec099aee.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "f0b8f13b84c5940d4222",
+    "url": "/static/css/main.194532ad.chunk.css"
+  },
+  {
+    "revision": "07d82178e48fca7133d0adcf194a6750",
+    "url": "/index.html"
+  }
+];
